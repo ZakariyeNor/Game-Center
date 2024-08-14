9f7578fb1df258137b7d3ab766bb1619e90320fa
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-08-14 13:33:24 +0000

    add quiz-area after clicking the let's try button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@
 
 const start = document.getElementById('strt');
 
+const quizQ = document.getElementById('quiz-q');
+
 start.addEventListener('click', quizDisplay)
 
 function quizDisplay() {
     start.classList.add('hid')
+    quizQ.classList.remove('hid')
 }
