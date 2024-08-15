ed79b9588a5197e710614780e9c572abeb6ceb46
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-08-15 19:25:39 +0000

    add event listener to the  first question's answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,20 @@ const start = document.getElementById('strt');
 
 const quizQ = document.getElementById('quiz-q');
 
+document.addEventListener('DOMContentLoaded', function() {
+    const answerChoices = document.querySelectorAll('.answer .opt');
+    const correctAnswerIndex = 2;
+
+    answerChoices.forEach((choice, index) => {
+        choice.addEventListener('click', function() {
+            if(index === correctAnswerIndex) {
+                choice.classList.add('correct');
+            } else {
+                choice.classList.add('incorrect');
+            }
+        });
+    });
+});
 
 let questionPosition = 0;
 
@@ -20,7 +34,5 @@ function displayNextQuestion() {
     showQuestion([questionPosition])
 }
 
-function selectAnswer(e) {
 
-}
 
