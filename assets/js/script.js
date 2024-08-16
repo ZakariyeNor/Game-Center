ee3cd9bf55092b9284fafe1788f331b81032ff9f
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-08-16 13:31:30 +0000

    add disabled attribute to prevent clicking more than once

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 
-
-const start = document.getElementById('strt');
-
-const quizQ = document.getElementById('quiz-q');
-
 document.addEventListener('DOMContentLoaded', function() {
+    const start = document.getElementById('strt');
+    const quizQ = document.getElementById('quiz-q');    
     const answerChoices = document.querySelectorAll('.answer .opt');
     const correctAnswerIndex = 2;
 
+    start.addEventListener('click', quizDisplay);
+
+    function quizDisplay() {
+        start.classList.add('hid')
+        quizQ.classList.remove('hid')
+        displayNextQuestion()
+    }
+
     answerChoices.forEach((choice, index) => {
         choice.addEventListener('click', function() {
+
+            answerChoices.forEach(btn => btn.setAttribute('disabled', true));
+
             if(index === correctAnswerIndex) {
                 choice.classList.add('correct');
             } else {
@@ -18,16 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
-
-start.addEventListener('click', quizDisplay)
-
-function quizDisplay() {
-    start.classList.add('hid')
-    quizQ.classList.remove('hid')
-    displayNextQuestion()
-}
 
 
 
