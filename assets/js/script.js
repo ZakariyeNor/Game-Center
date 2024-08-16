5f72be686602cff307032d6c2c858b09e2984a1b
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-08-16 14:34:01 +0000

    remove questionIndex to make the questions iterate and add currentQuestionIndex

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const start = document.getElementById('strt');
     const quizQ = document.getElementById('quiz-q');    
     const answerChoices = document.querySelectorAll('.answer .opt');
-    const correctAnswerIndex = 2;
+    let correntQuestionIndex = 0;
 
     start.addEventListener('click', quizDisplay);
 
     function quizDisplay() {
         start.classList.add('hid')
         quizQ.classList.remove('hid')
-        displayNextQuestion()
+        showQuestion(currentQuestionIndex);
     }
 
+    
     answerChoices.forEach((choice, index) => {
         choice.addEventListener('click', function() {
 
@@ -26,6 +27,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
 
